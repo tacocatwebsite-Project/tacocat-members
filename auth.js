d6c65c822b01d7d9fcb022598817fd60956457c2
tacocatwebsite-Project <tacocatwebsite@gmail.com>
2026-07-20 15:10:22 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = https://ztrgdltugiiityatvxpe.supabase.co";
 
+const SUPABASE_ANON_KEY = "sb_publishable_XPNsaBSY44-obehxipQ9mw_xSn0wVBN";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
