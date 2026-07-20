170a42e7f9399e8e3bcd9cff418ae89923a95360
tacocatwebsite-Project <tacocatwebsite@gmail.com>
2026-07-20 19:50:59 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
+const SUPABASE_URL =
+    "https://ztrgdltugiiityatvxpe.supabase.co";
 
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_XPNsaBSY44-obehxipQ9mw_xSn0wVBN";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
