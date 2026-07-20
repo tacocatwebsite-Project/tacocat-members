6957f9098f6e616b6c9dc01e662c4ae5ecb5b79a
tacocatwebsite-Project <tacocatwebsite@gmail.com>
2026-07-20 16:33:40 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = "https://ztrgdlttugiiityatvxpe.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_XPNsaBSY44-obehxipQ9mw_xSn0wVBN";
+const SUPABASE_URL = "https://ztrgdltugiiityatvxpe.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_XPNsaBSY44-obehxipQ9mw_xSn0wVBN";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
